595e271a7e18a3b7119033c294f3ca12213c50a2
Robert Gambee <robertgambee@gmail.com>
2016-02-07 14:09:22 -0500

    Add some error checking for hero attributes

--- Mob.py
+++ Mob.py
@@ -3,11 +3,38 @@ class Mob:
         pass
 
 class Hero(Mob):
-    def __init__(self, name, sex, role, alignment):
+    HERO_SEXES = ('female', 'male')
+    HERO_ROLES = ('archeologist', 'barbarian', 'caveperson', 'healer', 'knight', 'monk',
+                  'priest', 'ranger', 'rogue', 'samurai', 'tourist', 'valkyrie', 'wizard')
+    HERO_RACES = ('human', 'elf', 'dwarf', 'gnome', 'orc')
+    HERO_ALIGNMENTS = ('lawful', 'neutral', 'chaotic')
+    
+    def __init__(self, name, sex, role, race, alignment):
         self.name = name
-        self.sex = sex
-        self.role = role
-        self.alignment = alignment
+        
+        sex = sex.lower()
+        if (sex in HERO_SEXES):
+            self.sex = sex
+        else:
+            raise ValueError('Invalid sex: {}'.format(sex))
+        
+        role = role.lower()
+        if (role in HERO_ROLES:
+            self.role = role
+        else:
+            raise ValueError('Invalid role: {}'.format(role))
+        
+        race = race.lower()
+        if (race in HERO_RACES):
+            self.race = race
+        else:
+            raise ValueError('Invalid race: {}'.format(race))
+        
+        alignment = alignment.lower()
+        if (alignment in HERO_ALIGNMENTS):
+            self.alignment = alignment
+        else:
+            raise ValueError('Invalid alignment: {}'.format(alignment))
 
         self.level = 1
         self.HP = 100
