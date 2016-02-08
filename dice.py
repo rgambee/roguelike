313ec4dd7423c0aca131848c35709f82082a365b
Robert Gambee <robertgambee@gmail.com>
2016-02-07 23:19:40 -0500

    Add dice module

---
+++ dice.py
@@ -0,0 +1,19 @@
+import random
+
+def d4():
+    return random.randint(1, 4)
+
+def d6():
+    return random.randint(1, 6)
+
+def d8():
+    return random.randint(1, 8)
+
+def d10():
+    return random.randint(1, 10)
+    
+def d12():
+    return random.randint(1, 12)
+
+def d20():
+    return random.randint(1, 20)
